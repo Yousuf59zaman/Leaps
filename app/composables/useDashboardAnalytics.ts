604b418518eb379fb2be032104f27e5c5def1ac3
Yousuf59zaman <142763180+Yousuf59zaman@users.noreply.github.com>
2026-04-30 16:28:52 +0600

    refactor: remove unused dashboard analytics and filters composables

--- app/composables/useDashboardAnalytics.ts
+++
@@ -1,9 +0,0 @@
-type DashboardPageData = Record<string, unknown>
-
-export function useDashboardAnalytics(initialData: DashboardPageData = {}) {
-  const dashboardData = computed<DashboardPageData>(() => initialData)
-
-  return {
-    dashboardData
-  }
-}
